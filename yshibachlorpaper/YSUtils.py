132c5e9e904eedf0a3114c39a50a3c60cf1f1596
Taro <2271133020@qq.com>
2025-02-11 00:27:04 +0800

    拆分出没有翻译的文本

--- yshibachlorpaper/YSUtils.py
+++ yshibachlorpaper/YSUtils.py
@@ -10,3 +10,22 @@ def list_txt_files(folder_path):
                 file_path = os.path.join(root, file)
                 result.append(file_path)  # 返回文件的完整路径
     return result
+
+def list_files_path(folder_path, file_type):
+    result = []
+    # 遍历文件夹及其子文件夹中的所有文件
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(file_type):  # 只处理以 .'file_type' 结尾的文件
+                file_path = os.path.join(root, file)
+                result.append(file_path)  # 返回文件的完整路径
+
+    return result
+
+def list_files_name(folder_path, file_type):
+    result = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(file_type):
+                result.append(file)
+    return result
